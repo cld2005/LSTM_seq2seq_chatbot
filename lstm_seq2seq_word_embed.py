e39580a3445d753c43d9a7980d38dc1458178152
cld2005 <cld2005@live.cn>
2017-11-25 09:48:27 -0500

    push

--- lstm_seq2seq_word_embed.py
+++ lstm_seq2seq_word_embed.py
@@ -11,7 +11,7 @@ import os
 batch_size = 64  # Batch size for training.
 epochs = 30  # Number of epochs to train for.
 latent_dim = 512  # Latent dimensionality of the encoding space.
-num_samples = 1000  # Number of samples to train on.
+num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
 data_path = 'conv/codedak_conv.txt'
 BASE_DIR = '../'
